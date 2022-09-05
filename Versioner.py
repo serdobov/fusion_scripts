e44ed49d581d4f4bf414b00e5427cbd627c26e44
serdobov <anton.serdobov@gmail.com>
2022-09-05 12:02:15 +0300

    Moved all to pathlib

--- Versioner.py
+++ Versioner.py
@@ -5,6 +5,7 @@
 # script from the menu.
 
 import re
+from pathlib import Path, PurePath
 
 try:
     tool = comp.ActiveTool()  #Check that some tool is selected.
@@ -12,7 +13,7 @@ except:
     print("Error: You need to select a Loader.")
     exit()
 if tool.GetAttrs("TOOLS_RegID") == "Loader": #Check that selected tool is Loader
-    sourcePath = tool.GetInput("Clip") # Get path from lodaer
+    sourcePath = Path(tool.GetInput("Clip")) # Get path from lodaer
     globalIn = tool.GetInput("GlobalIn")  # Get loader start frame (probably 0, but who knows...)
     globalOut = tool.GetInput("GlobalOut") # Get loader end frame
     if comp.GetAttrs("COMPN_GlobalStart") != globalIn: # Check that comp IN equal source IN 
@@ -24,22 +25,21 @@ if tool.GetAttrs("TOOLS_RegID") == "Loader": #Check that selected tool is Loader
     if sourcePath == "":
         print("You need to set path to souce plate.")
         exit()
-    versionFind = re.search('[Vv]\d+', sourcePath)   # Parse version number from path
-    #versionNumber = re.search('\d+', versionFind.group())
+    versionFind = re.search('[Vv]\d+', str(sourcePath))   # Parse version number from path
     comp.Lock()
     allSavers = comp.GetToolList(False, "Saver")  # Get all Savers from current comp
     for currentSaver in allSavers:  # Evaluate thru all Savers one by one
-        currentSaverPath = allSavers[currentSaver].GetInput("Clip")   # Get path from current Saver
-        newSaverPath = re.sub('[Vv]\d+', versionFind.group(), currentSaverPath)  # Replace version token in path for a new version
-        allSavers[currentSaver].SetInput("Clip", newSaverPath)  # Set new path in current Saver node
-        print(currentSaverPath + ' -> ' + newSaverPath)
+        currentSaverPath = Path(allSavers[currentSaver].GetInput("Clip"))   # Get path from current Saver
+        newSaverPath = Path(re.sub('[Vv]\d+', versionFind.group(), str(currentSaverPath)))  # Replace version token in path for a new version
+        allSavers[currentSaver].SetInput("Clip", str(newSaverPath))  # Set new path in current Saver node
+        print(str(currentSaverPath) + ' -> ' + str(newSaverPath))
     print (str(len(allSavers)) + " entries changed successfully.")
-    oldCompPath = comp.GetAttrs("COMPS_FileName")  # Get current comp file path
-    newCompPath = re.sub('[Vv]\d+', versionFind.group(), oldCompPath) # Replace version 
+    oldCompPath = Path(comp.GetAttrs("COMPS_FileName"))  # Get current comp file path
+    newCompPath = re.sub('[Vv]\d+', versionFind.group(), str(oldCompPath)) # Replace version 
     comp.Unlock()
     try:
-        comp.Save(newCompPath)    # Save current comp tith new name
-        print("Comp " + newCompPath + " saved successfully.")
+        comp.Save(str(newCompPath))    # Save current comp tith new name
+        print("Comp " + str(newCompPath) + " saved successfully.")
     except:
         print("Error: can't save new composition version.")    
 else:
